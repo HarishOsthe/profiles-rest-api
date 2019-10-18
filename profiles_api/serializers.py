6c305a8358618d082e63a978b952ae08f9bbeeff
Harish Osthe <harishosthe@gmail.com>
2019-10-18 14:12:02 +0530

    added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
-
+from profiles_api import models
 
 class HelloSerializer(serializers.Serializer):
     """Serializs a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
     
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a User profile object"""
+    class Meta:
+        model=models.UserProfile
+        fields=("id","email","name","password")
+        extra_kwargs={
+            "password":{
+                "write_only":True,
+                "style":{
+                    'input_type':"password"
+                }
+            }
+        }
+        
+    def create(self,validated_data):
+        """Create and return a new user"""
+        user=models.UserProfile.objects.create_user(validated_data["email"], validated_data["name"], validated_data["password"])
+        return user
